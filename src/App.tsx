be7d3d992a966990d95904dcdd3acbe25832551b
Myroslav13 <myroslavshchuruk@gmail.com>
2025-08-05 21:18:14 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,14 @@ function App() {
 
       <div className='container position-relative z-1 bg-white rounded-4 p-4'>
         <div className='d-flex align-items-center gap-3'>
-          <img src='./assets/images/icon-star.svg'/>
+          <img src='assets/images/icon-star.svg'/>
           <h1 className='text-start fs-1 fw-bold'>FAQs</h1>
         </div>
 
         <div className='text-start section'>
           <div className='d-flex justify-content-between align-items-center pt-3 pb-2'>
             <h2 className='fs-6'>What is Frontend Mentor, and how will it help me?</h2>
-            <button className="btn" onClick={() => setStateButton1(!stateButton1)}><img src={`${stateButton1 === true ? "./assets/images/icon-minus.svg":"./assets/images/icon-plus.svg"}`}></img></button>
+            <button className="btn" onClick={() => setStateButton1(!stateButton1)}><img src={`${stateButton1 === true ? "assets/images/icon-minus.svg":"./assets/images/icon-plus.svg"}`}></img></button>
           </div>
 
           <p className={`${stateButton1 === true? 'd-block': "d-none"}`}>Frontend Mentor offers realistic coding challenges to help developers improve their 
@@ -32,7 +32,7 @@ function App() {
         <div className='text-start section'>
           <div className='d-flex justify-content-between align-items-center pt-3 pb-2'>
             <h2 className='fs-6'>Is Frontend Mentor free?</h2>
-            <button className="btn" onClick={() => setStateButton2(!stateButton2)}><img src={`${stateButton2 === true ? "./assets/images/icon-minus.svg":"./assets/images/icon-plus.svg"}`}></img></button>
+            <button className="btn" onClick={() => setStateButton2(!stateButton2)}><img src={`${stateButton2 === true ? "assets/images/icon-minus.svg":"./assets/images/icon-plus.svg"}`}></img></button>
           </div>
 
           <p className={`${stateButton2 === true? 'd-block': "d-none"}`}>Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
@@ -42,7 +42,7 @@ function App() {
         <div className='text-start section'>
           <div className='d-flex justify-content-between align-items-center pt-3 pb-2'>
             <h2 className='fs-6'>Can I use Frontend Mentor projects in my portfolio?</h2>
-            <button className="btn" onClick={() => setStateButton3(!stateButton3)}><img src={`${stateButton3 === true ? "./assets/images/icon-minus.svg":"./assets/images/icon-plus.svg"}`}></img></button>
+            <button className="btn" onClick={() => setStateButton3(!stateButton3)}><img src={`${stateButton3 === true ? "assets/images/icon-minus.svg":"./assets/images/icon-plus.svg"}`}></img></button>
           </div>
 
           <p className={`${stateButton3 === true? 'd-block': "d-none"}`}>Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
@@ -52,7 +52,7 @@ function App() {
         <div className='text-start section'>
           <div className='d-flex justify-content-between align-items-center pt-3 pb-2'>
             <h2 className='fs-6'>How can I get help if I'm stuck on a Frontend Mentor challenge?</h2>
-            <button className="btn" onClick={() => setStateButton4(!stateButton4)}><img src={`${stateButton4 === true ? "./assets/images/icon-minus.svg":"./assets/images/icon-plus.svg"}`}></img></button>
+            <button className="btn" onClick={() => setStateButton4(!stateButton4)}><img src={`${stateButton4 === true ? "assets/images/icon-minus.svg":"./assets/images/icon-plus.svg"}`}></img></button>
           </div>
           
           <p className={`${stateButton4 === true? 'd-block': "d-none"}`}>The best place to get help is inside Frontend Mentor's Discord community. There's a help 
